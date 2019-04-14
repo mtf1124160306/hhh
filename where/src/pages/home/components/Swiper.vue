036424f9df7d672bb7adacc65b3d9f6a32c9a8c5
mtf1124164306 <1124164306@qq.com>
2019-04-14 23:42:38 +0800

    add

---
+++ where/src/pages/home/components/Swiper.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <!--滚动                                             -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default{
+	name: "HomeSwiper",
+	data (){
+		return{
+			swiperOption: {}
+		}
+	}
+}
+</script>
+
+<style>
+	.swiper-img
+	   width: 100%
+</style>
